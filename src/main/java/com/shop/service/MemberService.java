24086adb884d74e9d66921648812d10bc4c5849e
Hyerin Lee <hyerinlee0v0@gmail.com>
2023-07-24 17:15:52 +0900

    css commit

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -39,6 +39,7 @@ public class MemberService implements UserDetailsService {
         }
 
         return User.builder().username(member.getEmail())
+                .username(member.getName())
                 .password(member.getPassword())
                 .roles(member.getRole().toString())
                 .build();
